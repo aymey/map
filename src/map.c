7db0d0092a554d67d0647c2dc057e3b7bbaa1ec7
aymey <fabianpaci@gmail.com>
2024-02-16 20:47:13 +1100

    hash function

--- src/map.c
+++ src/map.c
@@ -1,5 +1,6 @@
 #include "map.h"
 #include <stdlib.h>
+#include <stdint.h>
 
 Map *map_create(void) {
     Map *map = malloc(sizeof(Map));
@@ -23,4 +24,19 @@ void map_destroy(Map *map) {
 
     free(map->entries);
     free(map);
+}
+
+#define FNV_OFFSET 14695981039346656037UL
+#define FNV_PRIME 1099511628211UL
+
+// FNV-1a hash algorithm
+// https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash
+static uint64_t hash_key(const char *key) {
+    uint64_t hash = FNV_OFFSET;
+    for(const char *p = key; *p; p++) {
+        hash ^= (uint64_t)(unsigned char)(*p);
+        hash *= FNV_PRIME;
+    }
+
+    return hash;
 }
